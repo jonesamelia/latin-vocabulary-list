cbfdc38495d4a76fba23e3c3d7490468d4205fa2
Jake <NurkinJK@hendrix.edu>
2024-10-28 04:04:07 -0500

    Integrated PartOfSpeech (renamed WordType) into dialog via DropdownMenu

--- .history/to_dont_list/lib/widgets/to_do_dialog_20241027204855.dart
+++ .history/to_dont_list/lib/widgets/to_do_dialog_20241027204855.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
 
-enum PartOfSpeech{
-  v("Verb"),
-  n("Noun"),
-  adj("Adjective"),
-  adv("Adverb"),
-  c("Conjunction"),
-  p("Preposition"),
-  i("Interjection");
-
-  const PartOfSpeech(this.type);
-  final String type;
-}
-
 typedef ToDoListAddedCallback = Function(
     String value, String translatione, TextEditingController textController, TextEditingController trController);
 
@@ -32,12 +19,10 @@ class _ToDoDialogState extends State<ToDoDialog> {
   // Dialog with text from https://www.appsdeveloperblog.com/alert-dialog-with-a-text-field-in-flutter/
   final TextEditingController _wordController = TextEditingController();
   final TextEditingController _translationController = TextEditingController();
-  final TextEditingController _colorController = TextEditingController();
   final ButtonStyle yesStyle = ElevatedButton.styleFrom(
       textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.green);
   final ButtonStyle noStyle = ElevatedButton.styleFrom(
       textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.red);
-  PartOfSpeech? selectedPart = PartOfSpeech.v;
 
   String valueText = "";
   String translationText = "";
@@ -65,23 +50,6 @@ class _ToDoDialogState extends State<ToDoDialog> {
         controller: _translationController,
         decoration: const InputDecoration(hintText: "type translation here"),
       ),
-      const SizedBox(height: 12),
-      DropdownMenu<PartOfSpeech>(
-            initialSelection: PartOfSpeech.v,
-            controller: _colorController,
-            label: const Text('Faction'),
-            onSelected: (PartOfSpeech? part) {
-              setState(() {
-                selectedPart = part;
-              });
-            },
-            dropdownMenuEntries: PartOfSpeech.values.map<DropdownMenuEntry<PartOfSpeech>>((PartOfSpeech part) {
-              return DropdownMenuEntry<PartOfSpeech>(
-                value: part,
-                label: part.type,
-              );
-            }).toList(),
-          ),
       ] ),
       actions: <Widget>[
         ElevatedButton(
